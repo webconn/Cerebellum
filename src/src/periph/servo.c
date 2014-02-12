75d1cc8e5a8b99ecc3493d359eece9d01f8c9c67
Nikita 'webconn' Maslov <wkernelteam@gmail.com>
2014-02-12 22:23:05 +0400

    Remove extra legacy files
    Signed-off-by: Nikita 'webconn' Maslov <wkernelteam@gmail.com>

--- src/src/periph/servo.c
+++
@@ -1,8 +0,0 @@
-#include <pb/servo.h>
-
-void servo_write(uint8_t servo, uint8_t value)
-{
-    uart_send(CONFIG_PB_UART, 's');
-    uart_send(CONFIG_PB_UART, servo);
-    uart_send(CONFIG_PB_UART, value);
-}
