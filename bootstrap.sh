4b9809d49c995aff6d325887b0ec56c6f835a892
Nikita 'webconn' Maslov <wkernelteam@gmail.com>
2014-03-16 18:57:53 +0400

    bootstrap: Some decorations
    Signed-off-by: Nikita 'webconn' Maslov <wkernelteam@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,26 @@
 echo "Cerebellum library bootstrap for Antares"
 
 if [[ -n $ANTARES_INSTALL_DIR ]]; then
+
+        if [[ -a $ANTARES_INSTALL_DIR/src/lib/contrib/cerebellum ]]; then
+                echo "Looks like Cerebellum library is linked into Antares environment..."
+                echo "Nothing to be done."
+                exit
+        fi
+
         echo "Installing library for Antares..."
+
+        echo "Symlinking..."
+
         ln -sf `pwd`/src/ $ANTARES_INSTALL_DIR/src/lib/contrib/cerebellum
         ln -sf `pwd`/include/ $ANTARES_INSTALL_DIR/include/lib/cerebellum
+
+        echo "Add kcnf info..."
+
+        echo "source antares/src/lib/contrib/cerebellum/kcnf" >> $ANTARES_INSTALL_DIR/src/lib/contrib/kcnf
+
         echo "Done."
 else
         echo "Antares environment is not found!"
+        echo "Nothing to be done."
 fi
