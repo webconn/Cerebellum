b0291a11d9682db3694f28e12bdc29a888447c35
Nikita 'webconn' Maslov <wkernelteam@gmail.com>
2014-05-26 22:29:15 +0400

    gpio: try to create dynamic implementation
    Signed-off-by: Nikita 'webconn' Maslov <wkernelteam@gmail.com>

--- include/arch/avr/gpio.h
+++ include/arch/avr/gpio.h
@@ -1,22 +1,22 @@
-/**
- * Cerebellum Library GPIO expansion
- * helpers for AVR
- */
-
 #ifndef ANTARES_LIB_CONTRIB_CEREBELLUM_ARCH_AVR_GPIO_H
 #define ANTARES_LIB_CONTRIB_CEREBELLUM_ARCH_AVR_GPIO_H
 
+/**
+ * @file include/arch/avr/gpio.h
+ * @brief Arch-specific GPIO operations definitions for AVR
+ */
+
+/* Includes */
 #include <avr/io.h>
+/* Include list of GPIO expansion */
+#include "gpio_expansion.h"
 
 /**
- * Include list of GPIO expansion
- */
-#include <lib/cerebellum/arch/avr/gpio_expansion.h>
-
-/**
- * Raw macroses
+ * @addtogroup hal_gpio
+ * @{
  */
 
+/* Raw macros */
 #define GPIO_INIT_OUT_RAW(port, ddr, pin, num) do { ddr |= (1<<num); } while (0)
 #define GPIO_INIT_IN_RAW(port, ddr, pin, num) do { ddr &= ~(1<<num); port &= ~(1<<num); } while (0)
 #define GPIO_INIT_IN_PU_RAW(port, ddr, pin, num) do { ddr &= ~(1<<num); port |= (1<<num); } while (0)
@@ -31,16 +31,104 @@
 #define GPIO_TOGGLE_RAW(port, ddr, pin, num) (port ^= (1<<num))
 #endif
 
+/* Short macros */
+
 /**
- * Short macroses
+ * @brief Configure selected GPIO pin as output 
+ * @param GPIO GPIO pin description macro (GPxy)
+ */
+#define GPIO_INIT_OUT(gpio) GPIO_INIT_OUT_RAW(gpio)
+/** 
+ * @brief Configure selected GPIO pin as input
+ * @param GPIO GPIO pin description macro (GPxy)
+ */
+#define GPIO_INIT_IN(gpio) GPIO_INIT_IN_RAW(gpio)
+/** 
+ * Configure selected GPIO pin as input with pullup
+ * @param GPIO GPIO pin description macro (GPxy)
+ */
+#define GPIO_INIT_IN_PU(gpio) GPIO_INIT_IN_PU_RAW(gpio)
+/** 
+ * Read value from GPIO pin
+ * @param GPIO GPIO pin description macro (GPxy)
+ */
+#define GPIO_READ(gpio) GPIO_READ_RAW(gpio)
+/**
+ * Write 1 (HIGH) value into GPIO pin
+ * @param GPIO GPIO pin description macro (GPxy)
+ */
+#define GPIO_WRITE_HIGH(gpio) GPIO_WRITE_HIGH_RAW(gpio)
+/** 
+ * Write 0 (LOW) value into GPIO pin
+ * @param GPIO GPIO pin description macro (GPxy)
+ */
+#define GPIO_WRITE_LOW(gpio) GPIO_WRITE_LOW_RAW(gpio)
+/** 
+ * Toggle GPIO pin (change value from 0 to 1 or from 1 to 0)
+ * @param GPIO GPIO pin description macro (GPxy)
+ */
+#define GPIO_TOGGLE(gpio) GPIO_TOGGLE_RAW(gpio)
+
+/* Arch-specific typedefs (for performance) */
+
+/**
+ * @brief GPIO port enum
+ *
+ * Index of GPIO port for usage in gpio_pin_t structure.
+ */
+typedef enum {
+        /** reference to PORTA, PINA, DDRA */
+        #ifdef PORTA
+        GPIO_A,
+        #endif
+        /** reference to PORTB, PINB, DDRB */
+        #ifdef PORTB
+        GPIO_B,
+        #endif
+        /** reference to PORTC, PINC, DDRC */
+        #ifdef PORTC
+        GPIO_C,
+        #endif
+        /** reference to PORTD, PIND, DDRD */
+        #ifdef PORTD
+        GPIO_D = 3,
+        #endif
+        /** reference to PORTE, PINE, DDRE */
+        #ifdef PORTE
+        GPIO_E = 4,
+        #endif
+        /** reference to PORTF, PINF, DDRF */
+        #ifdef PORTF
+        GPIO_F = 5,
+        #endif
+        /** reference to PORTG, PING, DDRG */
+        #ifdef PORTG
+        GPIO_G = 6,
+        #endif
+        /** reference to PORTH, PINH, DDRH */
+        #ifdef PORTH
+        GPIO_H = 7
+        #endif
+} gpio_port_t;
+
+/**
+ * @brief GPIO modes enum
+ *
+ * List of supported GPIO modes in AVR
+ */
+typedef enum {
+        /** Output (push-pull) */
+        GPIO_MODE_OUT,
+        /** Input (Hi-Z state) */
+        GPIO_MODE_IN,
+        /** Input with Pull-up */
+        GPIO_MODE_IN_PU
+} gpio_mode_t;
+
+/**
+ * @}
  */
 
-#define GPIO_INIT_OUT(gpio) GPIO_INIT_OUT_RAW(gpio)
-#define GPIO_INIT_IN(gpio) GPIO_INIT_IN_RAW(gpio)
-#define GPIO_INIT_IN_PU(gpio) GPIO_INIT_IN_PU_RAW(gpio)
-#define GPIO_READ(gpio) GPIO_READ_RAW(gpio)
-#define GPIO_WRITE_HIGH(gpio) GPIO_WRITE_HIGH_RAW(gpio)
-#define GPIO_WRITE_LOW(gpio) GPIO_WRITE_LOW_RAW(gpio)
-#define GPIO_TOGGLE(gpio) GPIO_TOGGLE_RAW(gpio)
+
 
 #endif
--- include/gpio.h
+++ include/gpio.h
@@ -1,13 +1,14 @@
+#ifndef ANTARES_LIB_CONTRIB_CEREBELLUM_GPIO_H
+#define ANTARES_LIB_CONTRIB_CEREBELLUM_GPIO_H
+
 /**
  * @file include/gpio.h
  * @brief GPIO abstraction layer
  */
 
-#ifndef ANTARES_LIB_CONTRIB_CEREBELLUM_GPIO_H
-#define ANTARES_LIB_CONTRIB_CEREBELLUM_GPIO_H
-
 /**
- * GPIO abstraction layer
+ * @defgroup hal_gpio GPIO abstraction layer
+ * 
  *
  * You can find something close to this in Arduino, where
  * devs replaces port-pin notation with numbers.
@@ -15,12 +16,118 @@
  * In Cerebellum library, port-pin notation is not fully replaced,
  * but united in one structure to make system calls more
  * flexible.
+ * 
+ * GPIO HAL in Cerebellum library implements two ways to control GPIOs.
+ *
+ * <b>Tip</b>: These ways could be used together at same time.
+ *
+ * Way 1: Using Macros
+ * ===================
+ *
+ * This way is the best for time-critical applications because all GPIO operations
+ * will be parsed by C preprocessor and translated into "native" ASM instructions.
+ * But the disadvantage of this way is that you can't store GPIO address in memory.
+ *
+ * Each GPIO in MCU has a name which consists of port letter and pin number.
+ * Usually these names are written like PB3 (Port B pin 3). In Cerebellum library, these
+ * names are given like GPB3 (GPxy, if you want). First letter G is added to avoid
+ * conflicts with arch-specific C library (for example, in AVR-LIBC names Pxy are used
+ * for pin names).
+ *
+ * To perform some action with GPIO pin, use short macros.
+ *
+ * Way 2: Using Structures
+ * =======================
+ *
+ * This way is very flexible since each pin could be described as a couple of bytes in memory
+ * and you can easily store it, but this way is not such fast as first. But it still has
+ * its usecases (for easily configuring special libraries, to give GPIO as an argument into
+ * function, etc.)
+ *
+ * @example examples/avr/gpio/Blink/src/main.c
  */
 
 #include <stdint.h>
 
-#if defined(CONFIG_ARCH_AVR)
-#include <lib/cerebellum/arch/avr/gpio.h>
+#ifdef CONFIG_ARCH_AVR
+#include "arch/avr/gpio.h"
 #endif
+
+#include "arch/avr/generic.h"
+
+/**
+ * @addtogroup hal_gpio
+ * @{
+ */
+
+/**
+ * @brief GPIO pin description structure
+ * Used in dynamical GPIO abstraction
+ */
+struct gpio_pin {
+        /** GPIO port name */
+        gpio_port_t port;
+        /** GPIO pin number */
+        arch_uint_t pin;
+};
+
+/**
+ * @brief GPIO state typedef
+ * Used to set up GPIO value
+ */
+typedef enum {
+        /** Low value (set to GND) */
+        GPIO_LOW = 0,
+        /** High value (set to Vcc) */
+        GPIO_HIGH
+} gpio_state_t;
+
+/* GPIO functions implementations */
+
+/**
+ * @brief Initialize GPIO pin to run in given mode
+ *
+ * @param [in] pin GPIO pin description structure
+ * @param [in] mode GPIO operation mode
+ *
+ * @return Nothing to return
+ */
+void gpio_init(const struct gpio_pin *pin, gpio_mode_t mode);
+
+/**
+ * @brief Write given value into GPIO pin
+ *
+ * @param [in] pin GPIO pin description structure
+ * @param [in] state Required GPIO state
+ *
+ * @return Nothing to return
+ */
+void gpio_write(const struct gpio_pin *pin, gpio_state_t state);
+
+/**
+ * @brief Read the state of GPIO pin
+ *
+ * @param [in] pin GPIO pin description structure
+ *
+ * @return Current voltage level on GPIO pin
+ *
+ * @retval GPIO_LOW if pin level is low (GND)
+ * @retval GPIO_HIGH if pin level is high (Vcc)
+ */
+gpio_state_t gpio_read(const struct gpio_pin *pin);
+
+/**
+ * @brief Toggle GPIO pin
+ * Change GPIO output level from HIGH to LOW and from LOW to HIGH
+ *
+ * @param [in] pin GPIO pin description structure
+ *
+ * @return Nothing to return
+ */
+void gpio_toggle(const struct gpio_pin *pin);
+
+/**
+ * @}
+ */
 
 #endif
